9afa145497650e1c2b3d926f74fc843bb2266e0c
luan582266 <dotrieuluan0@gmail.com>
2025-07-30 18:01:11 +0700

    sửa lần n

--- README.md
+++
@@ -1,1 +0,0 @@
-# hit_python_public_2025
--- btvn_b2/bai2.py
+++
@@ -1,9 +0,0 @@
-luong = float(input("Nhập lương của bạn (đơn vị triệu đồng): "))
-if luong <= 7: 
-    thue = luong * 0.1
-elif 7 < luong <= 15: 
-    thue = luong * 0.2
-else: 
-    thue = luong * 0.3
-luong_rong = luong - thue
-print("Lương thực của nhân viên là: ", luong_rong, "triệu đồng")
